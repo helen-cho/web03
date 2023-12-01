078c3cb45e59ac8d789bbe3ab997aec46dcd2e39
helen-cho <hd9536@daum.net>
2023-12-01 12:44:06 +0900

    ver01

--- src/main/java/com/example/controller/ProRestController.java
+++ src/main/java/com/example/controller/ProRestController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,11 @@ public class ProRestController {
 	@PostMapping("/insert")
 	public void insert(@RequestBody ProVO vo) {
 		dao.insert(vo);
+	}
+	
+	@PostMapping("/delete/{pcode}")
+	public void delete(@PathVariable("pcode") String pcode) {
+		System.out.println("..............." + pcode);
+		dao.delete(pcode);
 	}
 }
